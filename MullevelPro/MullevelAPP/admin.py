183177cc7baaae28b234413b36c33da6d15b4359
giri79972 <48755929+giri79972@users.noreply.github.com>
2019-05-24 15:13:33 +0530

    Add files via upload

---
+++ MullevelPro/MullevelAPP/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Customer,Emp,Student
+# Register your models here.
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['cname','sales']
+class AdminEmp(admin.ModelAdmin):
+    list_display = ['ename','sal']
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['sname','marks']
+admin.site.register(Customer,AdminCustomer)
+admin.site.register(Emp,AdminEmp)
+admin.site.register(Student,AdminStudent)
---
+++ MullevelPro/MullevelAPP/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    cname=models.CharField(max_length=20)
+    sales=models.IntegerField()
+    def __str__(self):
+        return self.cname
+class Emp(Customer):
+    ename=models.CharField(max_length=20)
+    sal=models.IntegerField()
+    def __str__(self):
+        return self.ename
+class Student(Emp):
+    sname=models.CharField(max_length=20)
+    marks=models.IntegerField()
+    def __str__(self):
+        return self.sname
+
